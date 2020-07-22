ee192b8314791dec0a5bcc83b9268ed5ada6df61
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-07-22 02:38:36 -0700

    animation stuf

--- lib/widgets/mine.dart
+++ lib/widgets/mine.dart
@@ -1,54 +1,165 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 
 import '../models/mine_data.dart';
 
-class Mine extends StatelessWidget {
+class Mine extends StatefulWidget {
   final MineData mineData;
   final void Function() toggleMine;
 
-  Mine({
-    @required this.mineData,
-    @required this.toggleMine,
-  });
+  Mine({@required this.mineData, @required this.toggleMine});
+
+  @override
+  _MineState createState() => _MineState();
+}
+
+class _MineState extends State<Mine> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+
+  bool topLeft, topRight, bottomLeft, bottomRight;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+    topLeft = !widget.mineData.top && !widget.mineData.left;
+    topRight = !widget.mineData.top && !widget.mineData.right;
+    bottomLeft = !widget.mineData.bottom && !widget.mineData.left;
+    bottomRight = !widget.mineData.bottom && !widget.mineData.right;
+    // Now the issue is, corners need to take in account the topLeft ...
+    // tiles as well, not just the adjacent edge data.
+    //
+    // Figure it out, champ.
+  }
+
+  @override
+  void didUpdateWidget(Mine oldWidget) {
+    topLeft = !widget.mineData.top && !widget.mineData.left;
+    topRight = !widget.mineData.top && !widget.mineData.right;
+    bottomLeft = !widget.mineData.bottom && !widget.mineData.left;
+    bottomRight = !widget.mineData.bottom && !widget.mineData.right;
+
+    if (oldWidget.mineData.active != widget.mineData.active) {
+      if (widget.mineData.active)
+        _animationController.forward();
+      else {
+        _animationController.value = 1;
+        _animationController.reverse();
+      }
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 200),
-      curve: Curves.easeInOut,
-      padding: mineData.active
-          ? EdgeInsets.fromLTRB(
-              mineData.left ? 0 : 10,
-              mineData.top ? 0 : 10,
-              mineData.right ? 0 : 10,
-              mineData.bottom ? 0 : 10,
-            )
-          : EdgeInsets.zero,
-      child: GestureDetector(
-        onTap: toggleMine,
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 200),
-          curve: Curves.easeInOut,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: mineData.active && !mineData.top && !mineData.left
-                  ? Radius.circular(10)
-                  : Radius.zero,
-              topRight: mineData.active && !mineData.top && !mineData.right
-                  ? Radius.circular(10)
-                  : Radius.zero,
-              bottomLeft: mineData.active && !mineData.bottom && !mineData.left
-                  ? Radius.circular(10)
-                  : Radius.zero,
-              bottomRight:
-                  mineData.active && !mineData.bottom && !mineData.right
-                      ? Radius.circular(10)
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (_, __) => ClipPath(
+        clipper: CornerClipper(
+            topLeft, topRight, bottomLeft, bottomRight, 10 * _animation.value),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(
+            widget.mineData.left ? 0 : 10 * _animation.value,
+            widget.mineData.top ? 0 : 10 * _animation.value,
+            widget.mineData.right ? 0 : 10 * _animation.value,
+            widget.mineData.bottom ? 0 : 10 * _animation.value,
+          ),
+          child: GestureDetector(
+            onTap: widget.toggleMine,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: topLeft
+                      ? Radius.circular(10 * _animation.value)
                       : Radius.zero,
+                  topRight: topRight
+                      ? Radius.circular(10 * _animation.value)
+                      : Radius.zero,
+                  bottomLeft: bottomLeft
+                      ? Radius.circular(10 * _animation.value)
+                      : Radius.zero,
+                  bottomRight: bottomRight
+                      ? Radius.circular(10 * _animation.value)
+                      : Radius.zero,
+                ),
+                color: Theme.of(context).primaryColor,
+              ),
             ),
-            color: Theme.of(context).primaryColor,
           ),
         ),
       ),
     );
+  }
+}
+
+class CornerClipper extends CustomClipper<Path> {
+  double radius;
+  bool topLeft, topRight, bottomLeft, bottomRight;
+  CornerClipper(this.topLeft, this.topRight, this.bottomLeft, this.bottomRight,
+      this.radius);
+
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, topLeft ? radius : 0);
+    if (topLeft)
+      path.arcTo(
+        Rect.fromCircle(center: Offset.zero, radius: radius),
+        pi / 2,
+        -pi / 2,
+        false,
+      );
+
+    path.lineTo(size.width - (topRight ? radius : 0), 0);
+    if (topRight)
+      path.arcTo(
+        Rect.fromCircle(center: Offset(size.width, 0), radius: radius),
+        pi,
+        -pi / 2,
+        false,
+      );
+
+    path.lineTo(size.width, size.height - (bottomRight ? radius : 0));
+    if (bottomRight)
+      path.arcTo(
+        Rect.fromCircle(center: Offset(size.width, size.width), radius: radius),
+        3 * pi / 2,
+        -pi / 2,
+        false,
+      );
+
+    path.lineTo(bottomLeft ? radius : 0, size.height);
+    if (bottomLeft)
+      path.arcTo(
+        Rect.fromCircle(center: Offset(0, size.width), radius: radius),
+        0,
+        -pi / 2,
+        false,
+      );
+
+    path.lineTo(0, topLeft ? radius : 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CornerClipper oldClipper) {
+    return oldClipper.radius != this.radius;
   }
 }
--- lib/widgets/mines.dart
+++ lib/widgets/mines.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show max;
+
 import 'package:flutter/material.dart';
 import 'package:minesweeper/models/mine_data.dart';
 
@@ -17,27 +19,55 @@ class _MinesState extends State<Mines> {
   void toggleMine(int index) {
     final mineData = matrix[index];
 
-    var newLeft, newRight, newTop, newBottom;
+    final newMineData = MineData(
+      active: !mineData.active,
+    );
+    setState(() {
+      matrix[index] = newMineData;
+      recalculateSides();
+    });
+  }
 
-    final isTopLeft = index == 0;
-    final isTopRight = index == cols - 1;
+  void recalculateSides() {
+    print('recalculating sides...');
+    for (int index = 0; index < matrix.length; index++) {
+      final mineData = matrix[index];
 
-    final isBottomLeft = index == rows * cols - 1;
-    final isBottomRight = index == (rows - 1) * cols;
+      bool newLeft = false, newRight = false, newTop = false, newBottom = false;
+
+      if (index - 1 >= 0) {
+        newLeft = matrix[index - 1].active;
+      }
+      if (index + 1 < matrix.length) {
+        newRight = matrix[index + 1].active;
+      }
+      if (index - cols >= 0) {
+        newTop = matrix[index - cols].active;
+      }
+      if (index + cols < matrix.length) {
+        newBottom = matrix[index + cols].active;
+      }
+
+      matrix[index] = MineData(
+        active: mineData.active,
+        left: newLeft,
+        right: newRight,
+        top: newTop,
+        bottom: newBottom,
+      );
+    }
+
+    // final isTopLeft = index == 0;
+    // final isTopRight = index == cols - 1;
+
+    // final isBottomLeft = index == rows * cols - 1;
+    // final isBottomRight = index == (rows - 1) * cols;
 
     // if (index > 0) {
     //   newLeft = matrix[index - 1].active;
     // }
     // Basically need to do a check of each and every row and column I think,
     // A recursive approach might work as well but idk
-
-    final newMineData = MineData(
-      active: !mineData.active,
-      left: !mineData.left,
-    );
-    setState(() {
-      matrix[index] = newMineData;
-    });
   }
 
   @override
@@ -52,20 +82,133 @@ class _MinesState extends State<Mines> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: EdgeInsets.all(8.0),
-      shrinkWrap: true,
-      itemCount: matrix.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: cols,
-      ),
-      itemBuilder: (_, index) => AspectRatio(
-        aspectRatio: 1,
-        child: Mine(
-          mineData: matrix[index],
-          toggleMine: () => toggleMine(index),
+    return
+        // LayoutBuilder(
+        //   builder: (_, constraints) => SizedBox(
+        //     width: constraints.maxWidth - constraints.maxWidth % 10 + 1,
+        //     child:
+        AspectRatio(
+      aspectRatio: 5 / 7,
+      child: GridView.builder(
+        padding: EdgeInsets.all(8.0),
+        shrinkWrap: true,
+        itemCount: matrix.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: cols,
         ),
+        itemBuilder: (_, index) => AspectRatio(
+          aspectRatio: 1,
+          child: Mine(
+            mineData: matrix[index],
+            toggleMine: () => toggleMine(index),
+          ),
+        ),
+        //   ),
+        // ),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:minesweeper/models/mine_data.dart';
+
+// import 'mine.dart';
+
+// class Mines extends StatefulWidget {
+//   @override
+//   _MinesState createState() => _MinesState();
+// }
+
+// class _MinesState extends State<Mines> {
+//   final rows = 7;
+//   final cols = 5;
+
+//   List<MineData> matrix;
+
+//   void toggleMine(int index) {
+//     final mineData = matrix[index];
+
+//     final newMineData = MineData(
+//       active: !mineData.active,
+//     );
+//     setState(() {
+//       matrix[index] = newMineData;
+//       recalculateSides();
+//     });
+//   }
+
+//   void recalculateSides() {
+//     print('recalculating sides...');
+//     for (int index = 0; index < matrix.length; index++) {
+//       final mineData = matrix[index];
+
+//       bool newLeft = false, newRight = false, newTop = false, newBottom = false;
+
+//       if (index - 1 >= 0) {
+//         newLeft = matrix[index - 1].active;
+//       }
+//       if (index + 1 < matrix.length) {
+//         newRight = matrix[index + 1].active;
+//       }
+//       if (index - cols >= 0) {
+//         newTop = matrix[index - cols].active;
+//       }
+//       if (index + cols < matrix.length) {
+//         newBottom = matrix[index + cols].active;
+//       }
+
+//       matrix[index] = MineData(
+//         active: mineData.active,
+//         left: newLeft,
+//         right: newRight,
+//         top: newTop,
+//         bottom: newBottom,
+//       );
+//     }
+
+//     // final isTopLeft = index == 0;
+//     // final isTopRight = index == cols - 1;
+
+//     // final isBottomLeft = index == rows * cols - 1;
+//     // final isBottomRight = index == (rows - 1) * cols;
+
+//     // if (index > 0) {
+//     //   newLeft = matrix[index - 1].active;
+//     // }
+//     // Basically need to do a check of each and every row and column I think,
+//     // A recursive approach might work as well but idk
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     matrix = List<MineData>();
+
+//     for (int i = 0; i < rows * cols; i++) {
+//       matrix.add(MineData());
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AspectRatio(
+//       aspectRatio: 5 / 7,
+//       child: Wrap(
+//         children: [
+//           for (var index = 0; index < matrix.length; index++)
+//             FractionallySizedBox(
+//               widthFactor: 1 / cols,
+//               child: AspectRatio(
+//                 aspectRatio: 1,
+//                 child: Mine(
+//                   mineData: matrix[index],
+//                   toggleMine: () => toggleMine(index),
+//                 ),
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
